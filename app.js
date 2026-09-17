cc1ee538412d72bd09aa9d6da09869717ef2c707
maercioferraz81 <aulaskodular2024@gmail.com>
2026-09-17 11:57:10 -0300

    app.js porta

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.get("/", (req, res) => {
 });
 
 // SERVER
-app.listen(3000, () => {
-  console.log("http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
